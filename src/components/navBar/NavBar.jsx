8934057a5dc0c96d8262e31e59d882b31dc80540
Klippie <info@johanvanderklift.nl>
2023-01-04 12:07:23 +0100

    Work on navbar styling, and table drivers page

--- src/components/navBar/NavBar.jsx
+++ src/components/navBar/NavBar.jsx
@@ -1,10 +1,38 @@
 import React from 'react';
 import './NavBar.css'
+import logo from '../../assets/F1-logo.png';
+import fiaLogo from '../../assets/fia_logo.png'
+import userIcon from '../../assets/user-icon.png'
 
 function NavBar(props) {
   return (
-    <div>
-      <h2>NavBar</h2>
+    <div className="navbar-container">
+      <div className="navbar-inner-container">
+        <div className="navbar-logo"><img src={logo} alt="F1 logo"/></div>
+        <div className="navbar-content-container">
+          <div className="navbar-upper-container">
+            <span>j.klift@upcmail.nl</span>
+            <img src={userIcon} alt="User icon"/>
+          </div>
+          <div className="navbar-lower-container">
+            <ul className="nav-links">
+              <li>Drivers</li>
+              <li>Teams</li>
+              <li>Races</li>
+            </ul>
+            <ul className="nav-buttons">
+              <li>
+                {/*<button>Registreren</button>*/}
+                {/*<button>Log in</button>*/}
+                <button>Log out</button>
+              </li>
+              <li>
+                <img src={fiaLogo} alt="Fia logo"/>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
--- src/pages/drivers/Drivers.jsx
+++ src/pages/drivers/Drivers.jsx
@@ -1,11 +1,72 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import './Drivers.css';
+import axios from "axios";
 
-function Drivers(props) {
+function Drivers() {
+  const [drivers, setDrivers] = useState();
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(false);
+
+  useEffect(() => {
+    async function fetchDrivers() {
+      setLoading(true);
+      try {
+        setError(false);
+        const response = await axios.get("https://v1.formula-1.api-sports.io/rankings/drivers?season=2022", {
+          headers: {
+            "x-rapidapi-host": "v1.formula-1.api-sports.io",
+            "x-apisports-key": "2f029cd04292f27980387238737fd319",
+          }
+        })
+        // const response = await axios.get('https://jsonplaceholder.typicode.com/users');
+        console.log(response.data.response);
+        setDrivers(response.data.response);
+        // console.log(drivers);
+      } catch (e) {
+        console.error(e);
+        setError(true);
+      }
+      setLoading(false);
+    }
+
+    void fetchDrivers();
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [])
+
   return (
-    <div>
-      <h1>Drivers</h1>
-    </div>
+    <>
+      {loading && <span>Loading...</span>}
+      {error && <span>An error occurred while loading the data</span>}
+      <h1 className="drivers-header">Drivers</h1>
+      <div className="container">
+        {drivers &&
+          <table className="table table-drivers">
+            <thead>
+            <tr>
+              <th>Pos</th>
+              <th>Name</th>
+              <th>Picture</th>
+              <th>Team</th>
+              <th>Points</th>
+            </tr>
+            </thead>
+            <tbody>
+            {drivers.map((driver) => {
+              return (
+                <tr>
+                  <td>{driver.position}</td>
+                  <td><img src={driver.driver.image} alt=""/></td>
+                  <td>{driver.driver.name}</td>
+                  <td>{driver.team.name}</td>
+                  <td>{driver.points}</td>
+                </tr>
+              )
+            })}
+            </tbody>
+          </table>
+        }
+      </div>
+    </>
   );
 }
 
