adb2b065f529b1ba0654d1192fbd919667cbaff0
Klippie <info@johanvanderklift.nl>
2023-01-04 21:35:46 +0100

    Working on teams page, todo: handle images

--- src/pages/teams/Teams.jsx
+++ src/pages/teams/Teams.jsx
@@ -1,10 +1,82 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import './Teams.css';
-function Teams(props) {
+import axios from "axios";
+import noImageAvailable from '../../assets/No_image_available.svg.png'
+
+function Teams() {
+  const [teams, setTeams] = useState();
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(false);
+
+  useEffect(() => {
+    async function fetchDrivers() {
+      setLoading(true);
+      try {
+        setError(false);
+        const response = await axios.get("https://v1.formula-1.api-sports.io/teams", {
+          headers: {
+            "x-rapidapi-host": "v1.formula-1.api-sports.io",
+            "x-apisports-key": "2f029cd04292f27980387238737fd319",
+          }
+        })
+        // const response = await axios.get('https://jsonplaceholder.typicode.com/users');
+        console.log(response.data.response);
+        setTeams(response.data.response);
+        // console.log(drivers);
+      } catch (e) {
+        console.error(e);
+        setError(true);
+      }
+      setLoading(false);
+    }
+
+    void fetchDrivers();
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [])
+
+  function getImg(url) {
+    const img = new Image();
+    img.onload = function () {
+      return <img src={url} alt="Team logo"/>;
+    };
+    img.onerror = function () {
+      return <img src={noImageAvailable} alt="Not available"/>;
+    };
+    img.src = url;
+  }
+
   return (
-    <div>
-      <h1>Teams</h1>
-    </div>
+    <>
+      {loading && <span>Loading...</span>}
+      {error && <span>An error occurred while loading the data</span>}
+      <h1 className="teams-header">Teams</h1>
+      <div className="container">
+        {teams &&
+          <table className="table table-teams">
+            <thead>
+            <tr>
+              <th></th>
+              <th>Team</th>
+              <th>Director</th>
+              <th>Championships</th>
+            </tr>
+            </thead>
+            <tbody>
+            {teams.map((team) => {
+              return (
+                <tr>
+                  <td>{getImg(team.logo)}</td>
+                  <td>{team.name}</td>
+                  <td>{team.director}</td>
+                  <td>{team.world_championships ? team.world_championships : 0}</td>
+                </tr>
+              )
+            })}
+            </tbody>
+          </table>
+        }
+      </div>
+    </>
   );
 }
 
